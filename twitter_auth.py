24137a31dedd4be627c0d2fdb68ad62dbb895354
rohinb2 <bhasin.rohin@gmail.com>
2017-12-27 14:08:14 -0600

    change tweet count once more

--- twitter_auth.py
+++
@@ -1,7 +0,0 @@
-from tweepy import *
-
-class TwitterAuth:
-    # consumer key and secret
-    auth = AppAuthHandler("v3Ll0EaQJV5xf0V7FNaIyHXpq", "H1WPDAKMPlmuVESafHVfwpTJkxHZFilHPLWHQ4MjOloZSV8g4B")
-    # access token key and secret
-    #auth.set_access_token("1461196068-mibDysE7V9BNh6OyJZmMGr7wrh408zcXgOW3alf", "1jxzAAnb1YndqJeBwbMYTVq5oovxEemtvGfjz0s7Ajnft")
